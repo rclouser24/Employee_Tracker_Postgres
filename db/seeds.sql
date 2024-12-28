b8303bdf9e63751d5fc7b6fee7f3f1a38f340109
Clouser <clouser24ryan@gmail.com>
2024-12-28 13:02:35 -0500

    final changes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,24 @@
 -- Populate the department table
-INSERT INTO department (id, name)
-VALUES (1, 'Sales'),
-       (2, 'Engineering'),
-       (3, 'Finance'),
-       (4, 'Legal');
+INSERT INTO department (name)
+VALUES ('Sales'),
+       ('Engineering'),
+       ('Finance'),
+       ('Legal');
 
 -- Populate the role table
-INSERT INTO role (id, title, salary, department_id)
-VALUES (1, 'Salesperson', 100000, 1),
-       (2, 'Engineer', 120000, 2),
-       (3, 'Accountant', 85000, 3),
-       (4, 'Lawyer', 120000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Salesperson', 100000, 1),
+       ('Engineer', 120000, 2),
+       ('Accountant', 85000, 3),
+       ('Lawyer', 120000, 4);
 
 -- Populate the employee table
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, 'Robert', 'Hernandez', 1, NULL),
-       (2, 'John', 'Doe', 2, 1),
-       (3, 'Jane', 'Doe', 3, 2),
-       (4, 'Alice', 'Smith', 4, 3),
-       (5, 'Bob', 'Smith', 1, 4),
-       (6, 'Charlie', 'Smith', 2, 5),
-       (7, 'David', 'Smith', 3, 6),
-       (8, 'Eve', 'Smith', 4, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Robert', 'Hernandez', 1, NULL),
+       ('John', 'Doe', 2, 1),
+       ('Jane', 'Doe', 3, 2),
+       ('Alice', 'Smith', 4, 3),
+       ('Bob', 'Smith', 1, 4),
+       ('Charlie', 'Smith', 2, 5),
+       ('David', 'Smith', 3, 6),
+       ('Eve', 'Smith', 4, 7);
